da3b4225cd977abf7393673254b9fb8cac2f0323
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-02-25 23:44:55 +0530

    added test

--- es6.js
+++ es6.js
@@ -1,14 +1,14 @@
 let x = 1;
 {
-    let y = 2;
-    console.log(y)
+  let y = 2;
+  console.log(y);
 }
 
 {
-    let y = 5;
-    console.log(y)
+  let y = 5;
+  console.log(y);
 }
 console.log(y);
 console.log(x);
 
-
+// Test Clones
